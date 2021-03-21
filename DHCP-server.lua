74d08b6cc1acc45b079d4c6a68f26a494bca2c2c
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2021-03-21 10:55:13 +0100

    Create DHCP-server.lua

---
+++ DHCP-server.lua
@@ -0,0 +1,54 @@
+-- ID Example: 1.005 (network.machine)
+-- Machine 1 is always DHCP
+-- Machine 2 is dns
+local modem = peripheral.find("modem")
+local DHCPchannel = 1
+
+
+local allocated_ids = {}
+
+local init = function()
+  modem.open(DHCPchannel)
+  table.insert(allocated_ids,1)
+end
+
+
+
+local isRegistered = function(ID)
+  for i,o in pairs(allocated_ids) do
+    if o == ID then
+      return true
+    end
+  end
+return false
+end
+
+
+local registerID = function()
+  local random = math.random(2,255)
+
+  if isRegistered(random) then
+    registerIP()
+    print("Found ID but was already taken")
+    return
+  end
+  print("Found ID: " .. random)
+  table.insert(allocated_ids,random)
+
+  return random
+end
+
+
+init()
+
+
+while true do
+  local event, side, ch, rch, msg, dist = os.pullEvent("modem_message")
+  if msg == "REGISTER" then
+    local ID = registerID()
+    if not rch then
+      rch = DHCPchannel
+    end
+    modem.transmit(rch,DHCPchannel,ID)
+  end
+end
